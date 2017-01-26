f3b6c6f3e814ae94d97f6fa5644f0975b7cf2866
Sándor Nagy <privsala@gmail.com>
2017-01-27 00:02:57 +0100

    Added openweathermap integration and temperature return

--- src/introsde/ws/WeatherImpl.java
+++ src/introsde/ws/WeatherImpl.java
@@ -1,4 +1,5 @@
 package introsde.ws;
+
 import java.net.URI;
 
 import javax.jws.WebService;
@@ -6,48 +7,58 @@ import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
 import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 
 import org.glassfish.jersey.client.ClientConfig;
+
+import org.json.JSONObject;
 
 
 //Service Implementation
 @WebService(endpointInterface = "introsde.ws.Weather")
 public class WeatherImpl implements Weather {
+	/**
+	 * getWeatherInformationByCity(String city)
+	 */
     @Override
-    public String getWeatherInformationByCity(String name) {
+    public String getWeatherInformationByCity(String city) {
         
+    	System.out.println("getWeatherInformationByCity called with argument: " + city);
         // Fetch the weather information for the Adapter to return
-        /*Integer temperature = -100;
+        String result = "Init";
         
-        String mediaType = MediaType.APPLICATION_JSON;
-        // http://api.openweathermap.org/data/2.5/weather?q=Budapest,hu&appid=7c73b4b9e7e29e9c0335c81ccdd8352d&units=Metric
-        String url = "http://localhost:5900/sdelab";
-		ClientConfig clientConfig = new ClientConfig();
-		Client client = ClientBuilder.newClient(clientConfig);
-		WebTarget service = client.target(getBaseURI(url));
-        WebTarget resourceWebTarget = service.path("person/1");
-		Response r = resourceWebTarget.request().accept(mediaType).get(Response.class);
+        try {
+	        String mediaType = MediaType.APPLICATION_JSON;
+	        // http://api.openweathermap.org/data/2.5/weather?q=Budapest,hu&appid=7c73b4b9e7e29e9c0335c81ccdd8352d&units=Metric
+	        String url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=7c73b4b9e7e29e9c0335c81ccdd8352d&units=Metric";
+	        System.out.println("Request url: " + url);
+			ClientConfig clientConfig = new ClientConfig();
+			Client client = ClientBuilder.newClient(clientConfig);
+			WebTarget service = client.target(getBaseURI(url));
+	        //WebTarget resourceWebTarget = service.path("data/2.5/weather?q=" + city + "&appid=7c73b4b9e7e29e9c0335c81ccdd8352d&units=Metric");
+			Response r = service.request().accept(mediaType).get(Response.class);
+			
+			if(r.getStatus() == 200) {
+				result = ""; //new String();
+				String output = r.readEntity(String.class);
+				JSONObject json_data = new JSONObject();
+				json_data = new JSONObject(output);
+				Integer temp = json_data.getJSONObject("main").getInt("temp");
+				result += temp;
+				System.out.println(temp);
+				System.out.println(output);
+				System.out.println("--> Request done OK");
+			} else {
+				System.out.println(r.getStatus());
+				System.out.println(r.getLocation());
+				System.out.println(r.readEntity(String.class));
+			}
+        } catch (Exception e) {
+        	result = "Exception: " + e.toString();
+		}
 		
-		if(r.getStatus() == 200) {
-			String output = r.readEntity(String.class);
-			System.out.println(output);
-			System.out.println("-- OK -- User fetched successfully.");
-		} else {
-			System.out.println(r.getStatus());
-		}*/
-		
-		return "Temperature: " + name;
+		return result;
     }
     
     private static URI getBaseURI(String uriServer) {
