a80ba6229a09f6c72fa28363f113c8b2f94c72ee
Sándor Nagy <privsala@gmail.com>
2017-01-27 00:02:17 +0100

    Reformating, manage import and refactor code

--- src/introsde/endpoint/WeatherPublisher.java
+++ src/introsde/endpoint/WeatherPublisher.java
@@ -8,27 +8,31 @@ import java.io.IOException;
 
 //Endpoint publisher
 public class WeatherPublisher{
-    /*public static void main(String[] args) {
-       Endpoint.publish("http://localhost:9003/ws/weather", new WeatherImpl());
-    }*/
     
     public static void main(String[] args) throws IllegalArgumentException, IOException, URISyntaxException{
         String PROTOCOL = "http://";
         String HOSTNAME = InetAddress.getLocalHost().getHostAddress();
-        if (HOSTNAME.equals("127.0.0.1"))
+        System.out.println(HOSTNAME);
+        if (HOSTNAME.equals("127.0.0.1") || HOSTNAME.equals("127.0.1.1"))
         {
             HOSTNAME = "localhost";
         }
         String PORT = "9003";
         String BASE_URL = "/ws/weather";
 
-        if (String.valueOf(System.getenv("PORT")) != "null"){
-            PORT=String.valueOf(System.getenv("PORT"));
+        // We need this so the App will run on Heroku properly where we got the assigned port
+        // in an environment value named PORT
+        if(String.valueOf(System.getenv("PORT")) != "null") {
+            PORT = String.valueOf(System.getenv("PORT"));
         }
 
-        String endpointUrl = PROTOCOL+HOSTNAME+":"+PORT+BASE_URL;
-        System.out.println("Starting People Service...");
-        System.out.println("--> Published at = "+endpointUrl);
+        // Assemble the final endpoint URL
+        String endpointUrl = PROTOCOL + HOSTNAME + ":" + PORT + BASE_URL;
+        // End publish the endpoint
         Endpoint.publish(endpointUrl, new WeatherImpl());
-        }
+        
+        
+        System.out.println("Starting People Service...");
+        System.out.println("--> Published at = " + endpointUrl);
+    }
 }
